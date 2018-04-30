9d3a00016147ad99d4f9e10d76f23328047d20fa
Dmitry Kravchenko <dmitriyk813@gmail.com>
2018-04-30 20:07:27 +0300

    fix

--- parallel_labs/lab_2/Integral.cs
+++ parallel_labs/lab_2/Integral.cs
@@ -6,37 +6,40 @@ using System.Threading.Tasks;
 
 namespace lab_2
 {
-    internal class Integral //sqrt(x^2)+1
+    internal static class Integral //sqrt(x^2)+1
     {
         static double TrapMethod(List<double> argList)
         {
-            return (Math.Sqrt(Math.Pow(argList[0],2)) + Math.Sqrt(Math.Pow(argList[1],2) + 2) * argList[2]/ 2);
+            return ((Math.Sqrt(Math.Pow(argList[0],2)) + Math.Sqrt(Math.Pow(argList[1],2) + 2)) * argList[2]/ 2);
         }
         
         public static void Main(string[] args)
         {
-            double a = Double.Parse(Console.ReadLine());
-            double b = Double.Parse(Console.ReadLine());
-            int n = int.Parse(Console.ReadLine());
-            Stopwatch sw = new Stopwatch();
-            sw.Start();
-            double h = (b - a) / n;
-            double[] x = new double[n];
+            double a = double.Parse(Console.ReadLine()); //from 
+            double b = double.Parse(Console.ReadLine()); //to
+            int n = int.Parse(Console.ReadLine()); //number of points
+            
+            double h = (b - a) / n; //step
+            
+            double[] x = new double[n]; //points
             for (int i = 0; i < n; i++)
             {
-                x[i] = a + i * h;
+                x[i] = a + (i * h);
             }
             
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            
             Task<double>[] tasks = new Task<double>[n];
-
+            //double result = 0.0;
             for (int i = 0; i < n-1; i++)
             {
-                List<double> ls = new List<double>();
-                ls.Add(x[i]);
-                ls.Add(x[i+1]);
-                ls.Add(h);
+                List<double> ls = new List<double> {x[i], x[i+1], h};
+
                 tasks[i] = new Task<double>(z => TrapMethod((List<double>)z),ls);
                 tasks[i].Start();
+
+                //result += TrapMethod(ls);
             }
 
             double res = 0.0;
@@ -46,6 +49,7 @@ namespace lab_2
             }
             sw.Stop();
 
+            
             Console.WriteLine("integral sqrt(x^2)+1 = "+res+"\ntime passed = "+sw.ElapsedMilliseconds+"ms");
             Console.ReadKey();
         }
